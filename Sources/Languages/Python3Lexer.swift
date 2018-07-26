890ad21f7e02780eb38585822b31aadca1aa345d
Stefan Wijnja <s.wijnja@me.com>
2018-07-27 00:24:48 +0200

    Add Python3 support

---
+++ Sources/Languages/Python3Lexer.swift
@@ -0,0 +1,53 @@
+//
+//  SwiftLexer.swift
+//  SourceEditor
+//
+//  Created by Louis D'hauwe on 24/07/2018.
+//  Copyright © 2018 Silver Fox. All rights reserved.
+//
+
+import Foundation
+import SavannaKit
+
+public class Python3Lexer: SourceCodeRegexLexer {
+	
+	public init() {
+		
+	}
+	
+	lazy var generators: [TokenGenerator] = {
+		
+		var generators = [TokenGenerator?]()
+		// Functions
+		generators.append(regexGenerator("\\bprint(?=\\()", tokenType: .identifier))
+		
+		generators.append(regexGenerator("(?<=[^a-zA-Z])\\d+", tokenType: .number))
+		
+		generators.append(regexGenerator("\\.\\w+", tokenType: .identifier))
+		
+		let keywords = "False None True and as assert break class continue def del elif else except finally for from global if import in is lambda nonlocal not or pass raise return try while with yield".components(separatedBy: " ")
+        
+		generators.append(keywordGenerator(keywords, tokenType: .keyword))
+		
+		// Line comment
+        generators.append(regexGenerator("#(.*)", tokenType: .comment))
+		
+		// Block comment or multi-line string literal
+		generators.append(regexGenerator("(\"\"\".*\"\"\")|(\'\'\'.*\'\'\')", options: [.dotMatchesLineSeparators], tokenType: .comment))
+
+		// Single-line string literal
+		generators.append(regexGenerator("('.*')|(\".*\")", tokenType: .string))
+
+		// Editor placeholder
+		var editorPlaceholderPattern = "(<#)[^\"\\n]*"
+		editorPlaceholderPattern += "(#>)"
+		generators.append(regexGenerator(editorPlaceholderPattern, tokenType: .editorPlaceholder))
+
+		return generators.compactMap( { $0 })
+	}()
+	
+	public func generators(source: String) -> [TokenGenerator] {
+		return generators
+	}
+	
+}
